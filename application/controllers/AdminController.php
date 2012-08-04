32fae0be04c971a27323a8c0175476ba898fbced
Lena Kondrateva <elena@konddrateva.com>
2012-08-04 23:52:14 +1200

    edit/delete post actions

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -16,11 +16,68 @@ class AdminController extends Zend_Controller_Action
 
     public function indexAction()
     {
+    	$request = $this->getRequest();
+    	$where = array();
+    	if (isset($request->search)) { //FIXME: temporary, only for assigment
+    		$search_text = strip_tags($request->search);
+    		$where[] = "title LIKE '%".$search_text."%' OR text LIKE '%".$search_text."%' OR author LIKE '%".$search_text."%'";
+    	
+    		$this->view->search_text = $search_text;
+    	}    	
+    	
         $mapper = new Application_Model_PostsMapper();
-        $posts = $mapper->fetchAll($where, $order_by);
+        $posts = $mapper->fetchAll($where);
         $this->view->posts = $posts;
+    }
+
+    public function posteditAction()
+    {
+    	$request = $this->getRequest();
+    	if ($request->post_id) {
+    		$post = new Application_Model_Posts();
+    		$mapper = new Application_Model_PostsMapper();
+    		$mapper->find($request->post_id, $post);
+    		 
+    		$form = new Application_Form_Post(array('post'=>$post));
+    		
+    		if ($request->isPost()) {
+    			if ($form->isValid($request->getPost())) {
+    				$data = $form->getValues();
+    		
+    				$post = new Application_Model_Posts($data);
+    				$mapper->save($post);
+    				return $this->_helper->redirector('index');
+    			}
+    		}
+    		
+    		$this->view->form = $form;
+    	}
+    }
+
+    public function postdeleteAction()
+    {
+        $request = $this->getRequest();
+    	if ($request->post_id) {
+    		$post = new Application_Model_Posts();
+    		$mapper = new Application_Model_PostsMapper();
+    		
+    		if ($request->delete) {
+    			$db = Zend_Db_Table::getDefaultAdapter();
+    			$where = $db->quoteInto('id = ?', $request->post_id);
+    			$db->delete('posts', $where);
+    			return $this->_helper->redirector('index');
+    		} else {
+    			$mapper->find($request->post_id, $post);
+    			$this->view->post = $post;
+    		}
+    	}
+        
     }
 
 
 }
+
+
+
+
 
---
+++ application/forms/Post.php
@@ -0,0 +1,63 @@
+<?php
+
+class Application_Form_Post extends Zend_Form
+{
+
+    protected $post;
+
+    public function setPost($post)
+    {
+        $this->post = $post;
+        return $this;
+    }
+
+    public function getPost()
+    {
+        return $this->post;
+    }
+
+    public function init()
+    {
+        $post = $this->post;
+
+        $this->setMethod('post');
+        $this->setAttrib('accept-charset', 'utf-8');
+        $required_suffix = ' * ';
+
+        $this->addElement('text', 'title', array(
+            'label'	=> 'Title',
+            'value' => $post->title
+        ));
+        $this->addElement('textarea', 'text', array(
+            'label'	=> 'Text',
+            'value' => $post->text
+        ));
+        $this->addElement('text', 'author', array(
+        		'label'	=> 'Author',
+        		'value' => $post->author
+        ));
+        $this->addElement('text', 'link', array(
+        		'label'	=> 'Link',
+        		'value' => $post->link
+        ));
+        $this->addElement('checkbox', 'is_favourite', array(
+        		'label'	=> 'Favourite',
+        		'value' => $post->is_favourite
+        ));
+        $this->addElement('text', 'date_added', array(
+        		'label'	=> 'Date added',
+        		'value' => $post->date_added
+        ));
+        $this->addElement('submit', 'submit', array(
+            'ignore'	=> true,
+            'label'		=> 'Save'
+        ));
+
+        $this->addElement('hidden', 'id', array(
+        	'value' => $post->id
+        ));
+    }
+
+
+}
+
