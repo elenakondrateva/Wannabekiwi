272e80e1b092e4378acca2b3cd84a76bccf90de8
Lena Kondrateva <elena@konddrateva.com>
2012-08-04 22:13:48 +1200

    admin zone and authentification

---
+++ application/controllers/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+class AdminController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+    	$auth = Zend_Auth::getInstance();
+    	if (!$auth->hasIdentity()) {
+    		return $this->_helper->redirector('auth', 'index');
+    	}
+    	$this->view->user = $auth->getIdentity();
+    	
+    	$this->_helper->layout->setLayout('admin');
+    }
+
+    public function indexAction()
+    {
+        $mapper = new Application_Model_PostsMapper();
+        $posts = $mapper->fetchAll($where, $order_by);
+        $this->view->posts = $posts;
+    }
+
+
+}
+
